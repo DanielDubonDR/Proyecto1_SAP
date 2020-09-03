01b9c46b65afb628711a37f33dcfd38a9de1b932
DanielRDubonR <danieldubon499@gmail.com>
2020-09-02 22:04:15 -0600

    Cree la estructura del cliente

--- src/Principal/Controlador/Cliente.java
+++ src/Principal/Controlador/Cliente.java
@@ -6,5 +6,57 @@ package Principal.Controlador;
  * @author Daniel Dubón
  */
 public class Cliente {
+    private String nombre, avatar;
+    private int edad, nit;
+    private char sexo;
+    
+    public Cliente(String nombre, int edad, char sexo, int nit, String avatar)
+    {
+        setNombre(nombre);
+        setEdad(edad);
+        setSexo(sexo);
+        setNit(nit);
+        setAvatar(avatar);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getNit() {
+        return nit;
+    }
+
+    public void setNit(int nit) {
+        this.nit = nit;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
     
 }
