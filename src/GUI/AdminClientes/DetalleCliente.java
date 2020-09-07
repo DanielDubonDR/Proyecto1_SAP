f16fb7b35662065b7121fb5fbe3b5afba749431b
DanielRDubonR <danieldubon499@gmail.com>
2020-09-06 20:02:49 -0600

    cree detalle cliente2

--- src/GUI/AdminClientes/DetalleCliente.java
+++ src/GUI/AdminClientes/DetalleCliente.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package GUI.AdminClientes;
+
+import javax.swing.JFrame;
 
 /**
  *
  * @author Daniel Dubón
  */
-public class DetalleCliente {
+public class DetalleCliente extends JFrame {
     
 }
